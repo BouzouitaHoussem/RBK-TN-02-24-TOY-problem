f96150419864b018f1b50c3e168c66edcd624e2e
HoussemBuzouita <houssembouzouita90@gmail.com>
2024-06-04 09:31:10 +0100

    done

--- firstNonRepeatedCharacter.js
+++ firstNonRepeatedCharacter.js
@@ -9,6 +9,30 @@
  * @param {String} string 
  * @returns {String}
  */
+//for loop
 const firstNonRepeatedCharacter = function (string) {
-  // TODO: your solution here
+  for(var i=0;i<string.length;i++){
+    if(string.indexOf(string[i])===string.lastIndexOf(string[i])){
+      return string[i]
+    }
+    
+  }
+  for(var j=0;j<string.length;j++){
+      if(string.indexOf(string[j])!==string.lastIndexOf(string[j])){
+          return null
+      }
+  }
+  
+}
+//recursion : it only works for the first example
+const firstNonRepeatedCharacter2 = function (string) {
+  if(string.length===0){
+    return null
+  }
+  var index=string[0]
+  var leastString=string.slice(1)
+  if(leastString.includes(index)){
+    return firstNonRepeatedCharacter2(leastString)
+  }
+  return index
 }
