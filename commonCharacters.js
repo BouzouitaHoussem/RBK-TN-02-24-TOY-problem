15a4cc15a30be8a6b47e9608bb63c58d1f27d75f
HoussemBuzouita <houssembouzouita90@gmail.com>
2024-06-05 09:48:50 +0100

    done

--- commonCharacters.js
+++ commonCharacters.js
@@ -19,3 +19,18 @@
  * Should return null or empty string of there is no commonCharacters
 
  *
+
+*/
+var commonCharacters=function(string1,string2){
+ var string3=""
+ for(var i=0;i<string1.length;i++){
+    for(var j=0;j<string2.length;j++){
+        if(string1[i]===string2[j] && string1[i]!=' ' && !string3.includes(string1[i])){
+            string3=string3+string1[i]
+        }
+    }
+ }
+ return string3 || ""
+
+
+}
